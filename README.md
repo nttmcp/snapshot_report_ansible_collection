f9edec2b7dbdf60b8ee7d6f4f9f331a0a2adb2e4
Ken Sinfield <ken.sinfield@cis.ntt.com>
2020-02-05 10:22:15 -0500

    Added support for last snapshot stats. Updated ansible-doc

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # MCP Snapshot Management and Reporting using Ansible
 
-This collection of Playbooks can be used to enable the snapshot service on servers as well as report on snapshot status
-
-## Information
-**Version:** 1.0.0
+This collection contains a module (and templates) dedicated to reporting on Snapshot usage within the NTT Ltd. MCP
 
 **Author:** Ken Sinfield <ken.sinfield@cis.ntt.com>
 
@@ -11,9 +8,9 @@ This collection of Playbooks can be used to enable the snapshot service on serve
 
 ### Reporting System Requirements
 
-* <250 servers 2GB of RAM 2 vCPUs
-* 250-750 servers 4GB of RAM 2 vCPUs
-* \>750 servers 6GB of RAM 2 vCPUs
+* <250 servers 500 of RAM 1 vCPUs
+* 250-500 servers 1GB of RAM 1 vCPUs
+* \>500 servers 2GB of RAM 2 vCPUs
 * Running 2 simultaneous reports against 2 different MCPs: Double the RAM and vCPU count
 
 ### Ansible Version
@@ -22,7 +19,6 @@ This collection of Playbooks can be used to enable the snapshot service on serve
 
 ### Python Modules
 
-* jmespath
 * requests
 * configparser
 * PyOpenSSL
@@ -41,7 +37,7 @@ Additionally, the NTTMCP-MCP Ansible collection is required.
 
 https://galaxy.ansible.com/nttmcp/mcp
 
-> ansible-galaxy collection install nttmcp.mcp
+> ansible-galaxy collection install nttmcp.mcp -f
 
 
 ## Authentication
@@ -66,40 +62,15 @@ Use environment variables:
     set -o history
 
 
-## Playbooks
+## Usage
 
-* **snapshot_report.yml** -- Use to generate a report showing servers with snapshots enabled and any failed snapshots
+* **kensinfield.snapshot_report.report** -- Use to generate a report showing servers with snapshots enabled and any failed snapshots.
 
-* **snapshot_service.yml** -- Use to enable snapshots for a provided list of servers using meta data tags within Cloud Control
+* **region** and **datacenter** are required arguments while **network_domain** is optional
 
-* **snapshot_server_csv.yml** -- Use to enable snapshots for a provided list of servers via a CSV file (use example_server_input.csv as a template)
+* If **network_domain** is omitted then the module will report on all servers for the user's organization within the MCP
 
-### snapshot_report.yml
+* The module must be run from the collections folder where the templates/ and reports/ directories are located or the templates/ and reports/ folder from the collection must be copied to the directory from which the module will be run
 
-* Update **region** and **mcp** extra-vars to the desired region and MCP ID.
-* Optionally include a Cloud Network Domain with the variable **cnd** to limit the reporting to a specific Cloud Network Domain. Otherwise all servers within the MCP will be reported on
-
-> snapshot_report.yml -e"region=na mcp=NA9"
-
-### snapshot_service.yml
-
-* Use with caution still in beta
-* Update **region**, **mcp** (MCP ID where the servers are hosted), **rep_mcp** (MCP ID to be used for replication)
-* Optionally include a Cloud Network Domain with the variable **cnd** to limit the reporting to a specific Cloud Network Domain. Otherwise all servers within the MCP will be reported on
-* Tag names by default are 'snapshot' for the snapshot plan name and 'window' for the start time of the snapshot
-* Default tag value mappings to Cloud Control snapshot plans can be found (and modified) in files/snapshot_config.json
-* No batching at this stage so do not use for large numbers of servers (e.g. more than 15 at a time)
-* Will skip servers that have already been enabled and had an initial snapshot taken (idempotent)
-* Monitor snapshot.log for progress reports
-
-> ansible-playbook snapshot_service.yml -e"region=na mcp=na9 rep_mcp=na12"
-
-### snapshot_service_csv.yml
-
-* Update **region**, **csv**, **primary_mcp** (MCP ID where the servers are hosted) and **secondary_mcp** (MCP ID to be used for replication)
-* Takes an initial snapshot of the server once the service has successfully been enabled
-* By default will process servers in batches of 15 servers at a time and pause for 30min to allow for the initial manual snapshot to be taken before proceeding
-* Will skip servers that have already been enabled and had an initial snapshot taken (idempotent)
-* Monitor snapshot.log for progress reports
-
-> ansible-playbook snapshot_service_csv.yml -e"region=na csv=my_csv.csv primary_mcp=NA9 secondary_mcp=NA12"
+> ansible localhost -c local -m kensinfield.snapshot_report.report -a"region=<region> datacenter=<MCP ID> network_domain=<network domain name>"
+> ansible localhost -c local -m kensinfield.snapshot_report.report -a"region=na datacenter=NA9"
--- plugins/modules/report.py
+++ plugins/modules/report.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2019, NTT Ltd.
+# Copyright (c) 2020, NTT Ltd.
 #
 # Author: Ken Sinfield <ken.sinfield@cis.ntt.com>
 #
@@ -13,17 +13,87 @@ __metaclass__ = type
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
     'status': ['preview'],
-    'supported_by': 'NTT Ltd.'
+    'supported_by': 'Ken Sinfield <ken.sinfield@cis.ntt.com>'
 }
 
 DOCUMENTATION = '''
 ---
+module: report
+short_description: Generate a set of reports for servers within a specified MCP
+description:
+    - Generate reports for server snapshots
+    - Reports are generated from the templates/ directory and written to reports/
+version_added: "2.10"
+author:
+    - Ken Sinfield (@kensinfield)
+options:
+    region:
+        description:
+            - The geographical region
+        required: false
+        type: str
+        default: na
+    datacenter:
+        description:
+            - The datacenter name
+        required: true
+        type: str
+    network_domain:
+        description:
+            - The name of a Cloud Network Domain
+            - Used to limit the search space for servers. Without a network domain
+        required: false
+        type: str
+notes:
+    - Requires NTT Ltd. MCP account/credentials
+    - Requires nttmcp.mcp Ansible Galaxy Collection
+    - ansible-galaxy collection install nttmcp.mcp -f
+requirements:
+    - requests
+    - configparser
+    - pyOpenSSL
+    - netaddr
+'''
+
+EXAMPLES = '''
+- hosts: 127.0.0.1
+  connection: local
+  tasks:
+
+  - name: Run a report on all servers in NA12 in the CND myCND
+    kensinfield.snapshot_report.report:
+      region: na
+      datacenter: NA12
+      network_domain: myCND
+'''
+
+RETURN = '''
+msg:
+    description: A cool message
+    type: str
+    returned: always
+servers:
+    description: The number of servers found and processed
+    type: int
+    returned: success
+snapshot_servers:
+    description: The number of servers found with the snapshot service enabled
+    type: int
+    returned: success
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.nttmcp.mcp.plugins.module_utils.utils import get_credentials, get_regions
-from ansible_collections.nttmcp.mcp.plugins.module_utils.provider import NTTMCPClient, NTTMCPAPIException
-from jinja2 import Environment, FileSystemLoader
+try:
+    from ansible_collections.nttmcp.mcp.plugins.module_utils.utils import get_credentials, get_regions
+    from ansible_collections.nttmcp.mcp.plugins.module_utils.provider import NTTMCPClient, NTTMCPAPIException
+    HAS_NTTMCP = True
+except ImportError:
+    HAS_NTTMCP = False
+try:
+    from jinja2 import Environment, FileSystemLoader
+    HAS_JINJA2 = True
+except ImportError:
+    HAS_JINJA2 = False
 import os
 
 # Python3 workaround for unicode function so the same code can be used with ipaddress later
@@ -47,6 +117,11 @@ def main():
         supports_check_mode=True
     )
 
+    if not HAS_NTTMCP:
+        module.fail_json(msg='This module requires the nttmcp.mcp Ansible Galaxy collection')
+    if not HAS_JINJA2:
+        module.fail_json(msg='This module requires the jinja2 Python module')
+
     try:
         credentials = get_credentials(module)
     except ImportError as e:
@@ -64,6 +139,7 @@ def main():
     server_w_snapshot_count = 0
     total_snapshots = 0
     total_replica_snapshots = 0
+    snapshot_dates = list()
 
     # Check the region supplied is valid
     regions = get_regions()
@@ -78,7 +154,7 @@ def main():
     except NTTMCPAPIException as e:
         module.fail_json(msg=e.msg)
 
-    # Get the CND object based on the supplied name
+    # Get the CND object based on the supplied name (if applicable)
     try:
         if network_domain_name:
             network_domain = client.get_network_domain_by_name(name=network_domain_name, datacenter=datacenter)
@@ -90,6 +166,7 @@ def main():
     except (KeyError, IndexError, AttributeError, NTTMCPAPIException):
         module.fail_json(msg='Failed to locate the Cloud Network Domain - {0}'.format(network_domain_name))
 
+    # Get a list of servers to process, filtering on the CND if one is supplied
     try:
         servers = client.list_servers(datacenter, network_domain_id, None, None)
     except NTTMCPAPIException as e:
@@ -97,6 +174,7 @@ def main():
 
     server_count = len(servers)
 
+    # Iterate around the list of servers and create a new list of servers that have the snapshot service enabled
     for server in servers:
         if server.get('snapshotService') is not None:
             servers_w_snapshots.append({
@@ -108,6 +186,7 @@ def main():
                                        'family': server.get('guest', {}).get('operatingSystem', {}).get('family'),
                                        })
     server_w_snapshot_count = len(servers_w_snapshots)
+    # For those servers with the snapshot service enabled, iterate around this list and gather stats on the snapshots
     for server in servers_w_snapshots:
         snapshot_count = 0
         server_failed_snapshots = 0
@@ -121,6 +200,10 @@ def main():
                 snapshot_count = len(snapshots)
                 total_snapshots += len(snapshots)
                 for snapshot in snapshots:
+                    # Create a list of tuples of snapshot creation times and state to be used to determine the date
+                    # and state of the last snapshot later on
+                    if not snapshot.get('replica', False):
+                        snapshot_dates.append((snapshot.get('startTime'), snapshot.get('state')))
                     if snapshot.get('state') != 'NORMAL':
                         server_failed_snapshots += 1
                         failed_snapshots.append({
@@ -140,7 +223,8 @@ def main():
                         elif snapshot.get('type') == 'SYSTEM' and snapshot.get('replica'):
                             remote_snapshot += 1
                             total_replica_snapshots += 1
-
+                # This is now later on... we need to sort the list of tuples on the creation date (not the state)
+                snapshot_dates.sort(key=lambda tmp_tuple: tmp_tuple[1])
             snapshot_report.append({
                                    'name': server.get('name'),
                                    'id': server.get('id'),
@@ -148,14 +232,16 @@ def main():
                                    'count': snapshot_count,
                                    'failed': server_failed_snapshots,
                                    'local': local_snapshot,
-                                   'remote': remote_snapshot
+                                   'remote': remote_snapshot,
+                                   'last': snapshot_dates.pop()[0],
+                                   'last_state': snapshot_dates.pop()[1]
                                    })
-            # module.exit_json(report=snapshot_report)
-
         except (KeyError, NTTMCPAPIException) as e:
             failed_servers.append({'name': server.get('name'), 'id': server.get('id'), 'error': e})
 
+    # Report generation time
     try:
+        # Check stuff exists cause apparently it's the right thing to do
         if not os.path.exists('reports'):
             os.makedirs('reports')
         if not os.path.exists('templates'):
@@ -163,10 +249,13 @@ def main():
         env = Environment(
             loader=FileSystemLoader('templates')
         )
+        # Love me some hardcoding....... Will make these arguments at a later date
         summary_tmp = env.get_template('summary_report_csv.j2')
         failed_server_tmp = env.get_template('failed_server_report_csv.j2')
         failed_tmp = env.get_template('failed_report_csv.j2')
         server_tmp = env.get_template('server_report_csv.j2')
+        # Oh yeah some repetitive code time
+        # Generate the reports from the Jinja2 templates.
         f = open('reports/{0}_summary_report.csv'.format(datacenter), 'w')
         f.write('{0}\n'.format(summary_tmp.render(servers=server_count, snapshot_servers=len(servers_w_snapshots), snapshot_count=total_snapshots, snapshot_replica_count=total_replica_snapshots)))
         f.close()
@@ -184,6 +273,7 @@ def main():
     except Exception as e:
         module.fail_json(msg='Could not template out the reports: {0}'.format(e))
 
+    # Hashtag goodtimes
     module.exit_json(msg="Success", servers=server_count, snapshot_servers=server_w_snapshot_count)
 
 
